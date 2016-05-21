8cb7c4a66cced6e8b82745c9e4cf716be269b364
Ulf Rossäng <ulf@rosscom.org>
2016-05-21 11:30:06 +0200

    implement family to listDetail entity

--- shopper/src/main/java/se/rosscom/shopper/business/list/entity/ListDetail.java
+++ shopper/src/main/java/se/rosscom/shopper/business/list/entity/ListDetail.java
@@ -8,10 +8,12 @@ package se.rosscom.shopper.business.list.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import se.rosscom.shopper.business.family.entity.Family;
 
 /**
  *
@@ -29,13 +31,44 @@ public class ListDetail {
     @Id
     @GeneratedValue
     private long id;
+        
+    @ManyToOne
+    private Family family;
+
     private String item;
 
-    public ListDetail(String item) {
+    public ListDetail(Family family, String item) {
+        this.family = family;
         this.item = item;
     }
 
     public ListDetail() {
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Family getFamily() {
+        return family;
+    }
+
+    public void setFamily(Family family) {
+        this.family = family;
+    }
+    
+    
     
 }
