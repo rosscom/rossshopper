9fe5b4395bb068b812e881bbe98e9dec02714ded
Joakim Rossäng <joakimrossang@Joakims-MacBook-Pro-3.local>
2017-03-15 19:09:11 +0100

    Imported ping functionality and added simple web standards front-end.

---
+++ shopper-ui/src/app.js
@@ -0,0 +1,17 @@
+class App {
+
+    constructor() {
+        console.log("initilized...");
+    }
+
+    pingServer() {
+        fetch('http://localhost:8080/shopper/api/ping')
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(error => console.error(error));
+    }
+
+}
+
+let app = new App();
+app.pingServer();
