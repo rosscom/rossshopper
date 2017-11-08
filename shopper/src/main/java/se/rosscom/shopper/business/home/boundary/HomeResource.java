130bf1520334645ecefd7a8ac384b3cad7e2b7b7
Joakim Rossäng <joakim.rossang@gmail.com>
2017-11-08 13:58:23 +0100

    Added json response to family all resource.

--- shopper/src/main/java/se/rosscom/shopper/business/home/boundary/HomeResource.java
+++ shopper/src/main/java/se/rosscom/shopper/business/home/boundary/HomeResource.java
@@ -9,13 +9,9 @@ import java.net.URI;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import se.rosscom.shopper.business.account.entity.Account;
@@ -48,6 +44,7 @@ public class HomeResource {
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Home> all() {
         return homeService.all();
     }
